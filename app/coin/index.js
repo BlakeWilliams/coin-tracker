92b262aca1ffa4928a9b4bec4a4618e85b92168b
Blake Williams <blake@blakewilliams.me>
2017-12-25 20:21:45 -0500

    Add click handler to open coinmarketcap page

--- app/coin/index.js
+++ app/coin/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Price from './price';
+import { shell } from 'electron';
+
+const COIN_URL_BASE = 'https://coinmarketcap.com/currencies';
 
 class Coin extends React.Component {
+  openUrl() {
+    const { data } = this.props;
+    shell.openExternal(`${COIN_URL_BASE}/${data.id}`);
+  }
+
   render() {
     const { data } = this.props;
     const symbol = data.symbol.toLowerCase();
 
     return (
-      <div className="coin-container__coin">
+      <div onClick={() => this.openUrl()} className="coin-container__coin">
         <img
           className="icon"
           src={`cryptocurrency-icons/svg/color/${symbol}.svg`}
